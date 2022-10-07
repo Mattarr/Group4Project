27df1387417374d20d40000e692323723cd06a65
Mattarr <mattarr27@gmail.com>
2022-10-06 19:18:40 -0700

    Revert "added script to end tent"
    This reverts commit 04b5c0dc64c890c1210cc2006ceb01fadbba3066.

--- Group4Level/Assets/Scripts/FinishLine.cs
+++ Group4Level/Assets/Scripts/FinishLine.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
-    void OnTriggerEnter2D(Collider2D collision)
+    void OnTriggerEnter2D(Collider 2D collision)
     {
         if(collision.tag == "Player")
         {
