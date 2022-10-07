20a7b8590ebc316a1fa8159fc2b0402896eec95e
JohnnyR <113726617+BURG1E@users.noreply.github.com>
2022-10-07 00:43:00 -0700

    update12

--- Group4Level/Assets/Scripts/CameraFollow.cs
+++ Group4Level/Assets/Scripts/CameraFollow.cs
@@ -8,6 +8,8 @@ public class CameraFollow : MonoBehaviour
     [Range(1, 10)] 
     public float smoothFactor;
 
+    public Vector3 minValues, maxValue;
+    
     private void LateUpdate()
     {
         Follow();
@@ -16,7 +18,11 @@ public class CameraFollow : MonoBehaviour
     void Follow()
     {
         Vector3 targetPosition = target.position + offset;
-        Vector3 smoothPosition = Vector3.Lerp(transform.position, targetPosition, smoothFactor * Time.deltaTime);
+        Vector3 boundPosition = new Vector3(
+            Mathf.Clamp(targetPosition.x, minValues.x, maxValue.x),
+            Mathf.Clamp(targetPosition.y, minValues.y, maxValue.y),
+            Mathf.Clamp(targetPosition.z, minValues.z, maxValue.z));
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, boundPosition, smoothFactor * Time.deltaTime);
         transform.position = smoothPosition;
     }
 }
--- Group4Level/Assets/Scripts/PlayerMovement.cs
+++ Group4Level/Assets/Scripts/PlayerMovement.cs
@@ -8,11 +8,13 @@ public class PlayerMovement : MonoBehaviour
 {
     private float horizontal;
     private float speed = 10f;          //player speed
-    private float jumpingPower = 3f;   //player jumping power
+    private float jumpingPower = 5f;   //player jumping power
     private bool isFacingRight = true;
     private bool isJumping;
     private bool IsGrounded;
     private Animator anim;
+
+    public PlayerHealth health;
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
@@ -28,6 +30,7 @@ public class PlayerMovement : MonoBehaviour
         // platform branch add respawn point
         respawnPoint = transform.position;
         anim = GetComponent<Animator>();
+        
     }
     void Update()
     {
@@ -85,7 +88,11 @@ public class PlayerMovement : MonoBehaviour
         {
             rb.velocity = new Vector2(horizontal * speed, rb.velocity.y * 0.5f);
         }
-        
+        if (health.playerHealth <= 0)
+        {
+            transform.position = respawnPoint;
+            health.playerHealth = 3;
+        }
         //calls flip method
         Flip();
 
@@ -105,6 +112,8 @@ public class PlayerMovement : MonoBehaviour
         {
             respawnPoint = transform.position;
         }
+
+        
  
     }
 
@@ -127,7 +136,6 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    
 }
 
     
