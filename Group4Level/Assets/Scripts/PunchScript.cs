3cce432aeed1eff3598184d95792ffb05538a355
JohnnyR <113726617+BURG1E@users.noreply.github.com>
2022-10-06 22:27:24 -0700

    update

--- Group4Level/Assets/Scripts/PunchScript.cs
+++ Group4Level/Assets/Scripts/PunchScript.cs
@@ -7,7 +7,10 @@ using UnityEngine.Timeline;
 public class PunchScript : MonoBehaviour
 {
     public Animator animator;
-
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
+    public int attackDamage = 1;
     void Update()
     {
         //lets player punch
@@ -21,5 +24,30 @@ public class PunchScript : MonoBehaviour
     void Attack()
     {
         animator.SetTrigger("Attack");
+        
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            Debug.Log("we hit" + enemy.name);
+
+            if (enemy.CompareTag("GroundEnemy"))
+
+                enemy.GetComponent<enemyBehavior>().TakeDamage(attackDamage);
+
+            else if(enemy.CompareTag("FlyingEnemy"))
+            {
+
+                enemy.GetComponent<flyingBehavior>().TakeDamage(attackDamage);
+            }
+        }
+
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+            return;
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
     }
 }
